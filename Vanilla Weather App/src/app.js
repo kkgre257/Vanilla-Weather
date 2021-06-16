7ef014f4558366ecd7e5725eeba665ab75a0c542
Kindra Greenawalt <kindragreenawalt@Kindras-MacBook-Air.local>
2021-06-16 16:38:23 -0400

    added time function in js

--- Vanilla Weather App/src/app.js
+++ Vanilla Weather App/src/app.js
@@ -1,6 +1,6 @@
 let apiKey = "4bd70506db1d63914f72514fd997e5e8";
 
-let apiURL = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(url).then(dispalyTemperature);
 
@@ -19,4 +19,30 @@ function dispalyTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
